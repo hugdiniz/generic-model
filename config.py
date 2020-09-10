1b833a0787f18f4f5949afc778c75ff53576f8e3
Hugo Diniz Rebelo <hugo.rebelo@radixeng.com.br>
2020-09-10 15:50:26 -0300

    first commit

---
+++ config.py
@@ -0,0 +1,6 @@
+#DialogFlow static vars
+authPath     = "auth.json"
+project_id    = "eletrica-prototype-91787"
+language_code = "pt"
+
+
---
+++ controller.py
@@ -0,0 +1,72 @@
+from flask import Flask, request,abort
+from flask_cors import CORS
+from flask import jsonify
+import os
+
+app = Flask(__name__)
+#app = Flask(__name__,static_folder="static")
+cors = CORS(app)
+
+@app.route("/")
+def index():
+    return "Hello World"
+
+def login():	
+	if(request.args["hash"] =="e4ec1fc3fe931e4d933e6f3492b676d7" or request.form["hash"] =="e4ec1fc3fe931e4d933e6f3492b676d7"):
+		return True
+	else:
+		print(request.form)
+		print(request.args)
+		abort(401)
+		return False
+
+@app.route("/api/test", methods = ["GET"])
+def get_model():
+	login()
+
+	### Get Variables
+	k = 5
+
+	try:
+		if("k" in request.args):
+			k = int(request.args["k"])
+		
+		query = request.args["query"]
+	except:
+		return app.response_class(
+      response="query or k wrong formatted",
+      status=400,
+      mimetype='application/json'
+    )
+
+	### Execute any model
+	return jsonify(
+		{
+			"k": k,
+			"query": query
+		}
+	)
+
+def controller(app):
+	app.static_folder = 'static'
+	port = int(os.environ.get('PORT', 5000))
+	app.run(host='0.0.0.0',ssl_context='adhoc',port=port)
+	
+	##init_train_model()
+	##print("Training time: "+ str(time() - t0))
+	
+	
+
+@app.errorhandler(401)
+def denied(error):
+    return "401 Acess Denied"
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    return "500 Internal Server Error"
+
+
+if __name__ == "__main__":
+	controller(app)
+
