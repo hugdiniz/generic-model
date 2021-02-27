f0a0494e92146c696283f3e2a4513bc94bb174f7
Hugo Rebelo <hugdiniz@gmail.com>
2021-02-27 17:26:53 -0300

    Adicionando Traceback no campeonato

--- controller/campeonato/__init__.py
+++ controller/campeonato/__init__.py
@@ -3,6 +3,7 @@ from flask_restplus import Api, Resource, fields,reqparse
 from controller.class_server import *
 from model.liga import *
 from model.rodada import *
+import traceback
 
 
 @namespace_cartola.route('/campeonatos')
@@ -13,6 +14,7 @@ class CampeonatosController(Resource):
             rodada_atual = campeonatos[0].rodadas.__len__()
         except Exception as e:
             print(e)
+            traceback.print_exc()
             return app.response_class(
                 response="campeonatos not found in database",
                 status=400,
